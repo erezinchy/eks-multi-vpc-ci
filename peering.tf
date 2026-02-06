0e6259ebc95bff36d2ad80b3057791a05fd2745f
erezinchy <erezinchy@gmail.com>
2026-02-06 11:07:01 +0200

    fix vpc peering

--- peering.tf
+++ peering.tf
@@ -12,13 +12,13 @@ resource "aws_vpc_peering_connection" "sentinel_split" {
 # Gateway -> Backend
 resource "aws_route" "gateway_to_backend" {
   route_table_id            = module.vpc_gateway.private_route_table_id
-  destination_cidr_block    = var.backend_vpc_cidr  # 10.2.0.0/16
+  destination_cidr_block    = var.backend_vpc_cidr 
   vpc_peering_connection_id = aws_vpc_peering_connection.sentinel_split.id
 }
 
 # Backend -> Gateway
 resource "aws_route" "backend_to_gateway" {
   route_table_id            = module.vpc_backend.private_route_table_id
-  destination_cidr_block    = var.gateway_vpc_cidr  # 10.1.0.0/16
+  destination_cidr_block    = var.gateway_vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.sentinel_split.id
 }
--- security.tf
+++ security.tf
@@ -1,21 +1,20 @@
 # 1. Allow Backend Nodes to receive traffic from Gateway VPC
 resource "aws_security_group_rule" "backend_allow_gateway_ingress" {
   type              = "ingress"
-  from_port         = 8080 
-  to_port           = 8080
+  from_port         = 80    # LB Port
+  to_port           = 8080  # App targetPort
   protocol          = "tcp"
-  cidr_blocks       = [var.gateway_vpc_cidr] # Allow the entire Gateway VPC
+  cidr_blocks       = [var.gateway_vpc_cidr] 
   security_group_id = aws_eks_cluster.backend.vpc_config[0].cluster_security_group_id
-  description       = "Allow Sentinel Gateway Proxy to reach Backend Services"
+  description       = "Allow traffic for both LB (80) and App (8080)"
 }
 
-# 2. Allow Gateway Nodes to send traffic to Backend VPC (Egress)
+# 2. Allow Gateway Nodes to send traffic out
 resource "aws_security_group_rule" "gateway_to_backend_egress" {
   type              = "egress"
-  from_port         = 8080
-  to_port           = 8080
+  from_port         = 80
+  to_port           = 8080 
   protocol          = "tcp"
   cidr_blocks       = [var.backend_vpc_cidr]
   security_group_id = aws_eks_cluster.gateway.vpc_config[0].cluster_security_group_id
-  description       = "Allow Gateway Nodes to send requests to Backend VPC"
 }
